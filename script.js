e0b6bc10b27fb9ee1e280613fdc12460a830145d
CaroolGomes <137942117+CaroolGomes@users.noreply.github.com>
2026-01-11 17:31:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   if (window.location.pathname.includes("presente.html")) {
-    tratarPaginaPresente();
+    const params = new URLSearchParams(window.location.search);
+
+    const nome = params.get("nome");
+    const valor = params.get("valor");
+    const qr = params.get("qr");
+    const cartao = params.get("cartao");
+
+    document.getElementById("titulo-presente").innerText = `${nome} — R$ ${valor}`;
+    document.getElementById("qr-pix").src = qr;
+    document.getElementById("link-cartao").href = cartao;
   }
 });
-
-function tratarPaginaPresente() {
-  const params = new URLSearchParams(window.location.search);
-
-  const nome = params.get("nome");
-  const valor = params.get("valor");
-  const qr = params.get("qr");
-  const cartao = params.get("cartao");
-
-  const titulo = document.getElementById("titulo-presente");
-  const imagemQR = document.getElementById("qr-pix");
-  const linkCartao = document.getElementById("link-cartao");
-
-  if (!nome || !valor || !qr) {
-    titulo.innerText = "Presente não identificado";
-    imagemQR.style.display = "none";
-    linkCartao.style.display = "none";
-    return;
-  }
-
-  titulo.innerText = `${nome} — R$ ${valor}`;
-  imagemQR.src = qr;
-
-  iif (cartao && cartao !== "#") {
-  linkCartao.href = cartao;
-} else {
-  linkCartao.href = "#";
-}
-
- 
-
 
 
